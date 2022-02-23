13cffd4e8e292f46c6f8892ee83b97335ff58897
Rafael Angonese <rafael.angonese09@gmail.com>
2022-02-22 22:04:39 -0300

    :sparkles: feat: config ThemeProvider into _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import ThemeContainer from "../contexts/theme/ThemeContainer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeContainer>
+      <Component {...pageProps} />
+    </ThemeContainer>
+  );
 }
 
-export default MyApp
+export default MyApp;
