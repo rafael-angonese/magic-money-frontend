8558a81724bb772f3c645704cced29dffd38e5f7
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-03 20:33:28 -0300

    :globe_with_meridians: feat: translate yup validations to pt-br

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ThemeContainer from "../contexts/theme/ThemeContainer";
 
+import "../config/yup.locale.pt-br";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeContainer>
