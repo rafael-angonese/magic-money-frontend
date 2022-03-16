a002658374b58bff33ca61dd372c2bf1868a6fbe
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-15 21:33:20 -0300

    :wrench: feat: configure toastfy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+
 import ThemeContainer from "../contexts/theme/ThemeContainer";
 import { AuthProvider } from "../contexts/AuthContext";
 
+import "react-toastify/dist/ReactToastify.css";
 import "../config/yup.locale.pt-br";
 import Layout from "../components/Layout/Layout";
 
@@ -19,6 +22,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         )}
         {!isAuthenticatedRoutes && <Component {...pageProps} />}
       </AuthProvider>
+      <ToastContainer  theme="dark" />
     </ThemeContainer>
   );
 }
