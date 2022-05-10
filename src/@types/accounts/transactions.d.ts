35e688a4adcd6f5ad2fefedc1c1565c454938e1a
Rafael Angonese <rafael.angonese09@gmail.com>
2022-05-10 00:27:36 -0300

    :sparkles: feat: created transaction typescript interface

---
+++ src/@types/accounts/transactions.d.ts
@@ -0,0 +1,14 @@
+
+export interface ITransaction {
+  id: string
+  date: string
+  description: string
+  amount: number
+  type: string
+  category_id: string
+  user_id: string
+  account_id: string
+  bank_account_id: string
+  created_at: string
+  updatedAt: string
+}
