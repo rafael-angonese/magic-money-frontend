3a1d669d52a56353db73381e64b3305ab749992a
Rafael Angonese <rafael.angonese09@gmail.com>
2024-03-24 01:36:48 -0300

    :sparkles: Faet(hooks): add useDebouncedCallback hook

---
+++ src/hooks/use-debounced-callback.ts
@@ -0,0 +1,27 @@
+import { useRef } from 'react'
+
+import DEBOUNCE_DELAY from '@/constants/debounce-delay'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useDebouncedCallback = <T extends (...args: any) => any>(
+  callback: T,
+  delay?: number,
+) => {
+  const tmeoutRef = useRef<number | null>(null)
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function debouncedFn(...args: any) {
+    if (tmeoutRef.current) {
+      clearTimeout(tmeoutRef.current)
+    }
+
+    tmeoutRef.current = setTimeout(() => {
+      // eslint-disable-next-line n/no-callback-literal
+      callback(...args)
+    }, delay || DEBOUNCE_DELAY)
+  }
+
+  return debouncedFn
+}
+
+export default useDebouncedCallback
