017684c4705ec78accd1c38441d868e1bc7b6f63
Rafael Angonese <rafael.angonese09@gmail.com>
2024-03-24 23:29:27 -0300

    :coffin: Remove accordtion component

--- src/components/ui/accordion/accordion-root.tsx
+++
@@ -1,9 +0,0 @@
-import { Root } from '@radix-ui/react-accordion'
-
-export const AccordionRoot: React.FC<
-  React.ComponentPropsWithoutRef<typeof Root>
-> = ({ ...props }) => {
-  return <Root {...props} />
-}
-
-AccordionRoot.displayName = Root.displayName
