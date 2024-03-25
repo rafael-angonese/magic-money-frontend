1cf05bb63fd6e5b9962fe0fb05674a38cebbaf74
Rafael Angonese <rafael.angonese09@gmail.com>
2024-03-24 23:29:46 -0300

    :coffin: Refactor: remove avatar dead code

--- src/components/ui/avatar/avatar.tsx
+++
@@ -1,9 +0,0 @@
-import { AvatarFallback } from './avatar-fallback'
-import { AvatarImage } from './avatar-image'
-import { AvatarRoot } from './avatar-root'
-
-export const Avatar = {
-  Root: AvatarRoot,
-  Image: AvatarImage,
-  Fallback: AvatarFallback,
-}
