32ccaff7ad4fcfb5826b6ed1db21ecfed54209dc
Rafael Angonese <rafael.angonese09@gmail.com>
2024-05-27 23:17:41 -0300

    :sparkles: Feat(header): add user name info

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,18 +11,24 @@ import { useAuthStore } from '@/store/use-auth-store'
 import { useSidebarStore } from '@/store/use-sidebar'
 
 const Header: React.FC = () => {
-  const { logout } = useAuthStore()
+  const { logout, user } = useAuthStore()
   const { isOpen, setIsOpen } = useSidebarStore()
+
+  const userName = user?.name || ''
 
   return (
     <div className="border-b">
       <div className="flex h-16 items-center gap-6 px-6">
         <Logo className="hidden sm:block" />
 
-        <div className="block sm:hidden">
+        <span className="hidden sm:block">{userName}</span>
+
+        <div className="sm:hidden items-center flex gap-2">
           <IconButton hoverTitle="Menu" onClick={() => setIsOpen(!isOpen)}>
             <Menu />
           </IconButton>
+
+          <span>{userName}</span>
         </div>
 
         <div className="ml-auto flex items-center gap-2">
